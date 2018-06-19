7101a85e9b2006a4289124dd6b4a22f6a0b19e66
debugdump <hexing@debugdump.com>
2018-06-19 16:04:23 +0800

    update ns2009.c

--- drivers/input/touchscreen/ns2009.c
+++ drivers/input/touchscreen/ns2009.c
@@ -23,6 +23,8 @@
 #include <linux/input-polldev.h>
 #include <linux/input/touchscreen.h>
 #include <linux/i2c.h>
+#include <linux/jiffies.h>
+#include <linux/delay.h>
 
 /* polling interval in ms */
 #define POLL_INTERVAL	30
@@ -45,7 +47,7 @@
  * is sometimes not accurately 0.
  * This value is based on experiements.
  */
-#define NS2009_PEN_UP_Z1_ERR	80
+#define NS2009_PEN_UP_Z1_ERR	40
 
 struct ns2009_data {
 	struct i2c_client		*client;
@@ -63,19 +65,60 @@ static int ns2009_ts_read_data(struct ns2009_data *data, u8 cmd, u16 *val)
 
 	error = i2c_smbus_read_i2c_block_data(data->client, cmd, 2, raw_data);
 	if (error < 0)
+	{
+		printk("error = %d\n", error);
 		return error;
+	}
 
 	if (unlikely(raw_data[1] & 0xf))
+	{
+		printk("xxxxxxxxxxx\n");
 		return -EINVAL;
+	}
 
 	*val = (raw_data[0] << 4) | (raw_data[1] >> 4);
+	if(NS2009_READ_Z1_LOW_POWER_12BIT == cmd)
+	{
+		//printk("Z = %d", *val);
+	}
+	else if(NS2009_READ_X_LOW_POWER_12BIT == cmd)
+	{
+		//printk("	X = %d", *val);
+	}
+	else if(NS2009_READ_Y_LOW_POWER_12BIT == cmd)
+	{
+		//printk("		Y = %d", *val);
+	}
 
 	return 0;
 }
 
+void swap1(u16 *xp, u16 *yp)
+{
+    u16 temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+// A function to implement bubble sort
+void bubbleSort(u16 arr[], int n)
+{
+   int i, j;
+   for (i = 0; i < n-1; i++)      
+
+       // Last i elements are already in place   
+       for (j = 0; j < n-i-1; j++) 
+           if (arr[j] > arr[j+1])
+              swap1(&arr[j], &arr[j+1]);
+}
+
+static unsigned long last_jiffies = 0;
+static u16 last_x, last_y, distance;
+#define TOUCH_COUNT_TOTAL    5
+#define TOUCH_COUNT_VALID    3
 static int ns2009_ts_report(struct ns2009_data *data)
 {
-	u16 x, y, z1;
+	u16 x, y, z1[9], x1[TOUCH_COUNT_TOTAL], y1[TOUCH_COUNT_TOTAL], i, count, count2, x_sum, y_sum;
 	int ret;
 
 	/*
@@ -83,26 +126,108 @@ static int ns2009_ts_report(struct ns2009_data *data)
 	 * more investigation, so we only use z1 axis to detect pen down
 	 * here.
 	 */
-	ret = ns2009_ts_read_data(data, NS2009_READ_Z1_LOW_POWER_12BIT, &z1);
-	if (ret)
-		return ret;
-
-	if (z1 >= NS2009_PEN_UP_Z1_ERR) {
-		ret = ns2009_ts_read_data(data, NS2009_READ_X_LOW_POWER_12BIT,
-					  &x);
+	for(i=0;i<sizeof(z1)/sizeof(z1[0]);i++)
+	{
+		ret = ns2009_ts_read_data(data, NS2009_READ_Z1_LOW_POWER_12BIT, &z1[i]);
 		if (ret)
 			return ret;
+	}
 
-		ret = ns2009_ts_read_data(data, NS2009_READ_Y_LOW_POWER_12BIT,
-					  &y);
-		if (ret)
-			return ret;
 
+	if ((z1[0] >= NS2009_PEN_UP_Z1_ERR) && (z1[1] >= NS2009_PEN_UP_Z1_ERR) && (z1[2] >= NS2009_PEN_UP_Z1_ERR)
+		&&(z1[3] >= NS2009_PEN_UP_Z1_ERR) && (z1[4] >= NS2009_PEN_UP_Z1_ERR) && (z1[5] >= NS2009_PEN_UP_Z1_ERR)
+		&&(z1[6] >= NS2009_PEN_UP_Z1_ERR) && (z1[7] >= NS2009_PEN_UP_Z1_ERR) && (z1[8] >= NS2009_PEN_UP_Z1_ERR)
+	)
+	{
+		count = 0;
+		do
+		{
+			ns2009_ts_read_data(data, NS2009_READ_X_LOW_POWER_12BIT, &x1[count]);
+			ns2009_ts_read_data(data, NS2009_READ_Y_LOW_POWER_12BIT, &y1[count]);
+
+			if ((x1[count] > 3900) || (y1[count] > 3900))
+			{
+				printk(" > 3900 break");
+				//return -3;
+				break;
+			}
+
+			if((x1[count] < 80) || (y1[count] < 80))
+			{
+				printk(" < 80 break");
+				break;
+			}
+
+
+
+			distance = int_sqrt((last_x-x1[count])*(last_x-x1[count]) + (last_y-y1[count])*(last_y-y1[count]));
+			//printk("count=%d,x=%d,y=%d,last_j=%lu,current_j=%lu,diff=%lu,lastx=%d,lasty=%d, distance=%d", count, x, y, last_jiffies, jiffies, jiffies-last_jiffies, last_x, last_y, distance);
+			if((jiffies-last_jiffies <= 30) && (distance > 800))
+			{
+				printk("feidian 3: distance=%d", distance);
+				return -5;
+			}
+
+
+
+			count++;
+		}
+		while(count < TOUCH_COUNT_TOTAL);
+
+		if(count < TOUCH_COUNT_VALID)
+		{
+			printk("count < %d", TOUCH_COUNT_VALID);
+			return -1;
+		}
+
+		bubbleSort(x1, count);
+		bubbleSort(y1, count);
+		x_sum = 0;
+		y_sum = 0;
+		for(i=0;i<count;i++)
+		{
+			printk("\t\t%d: x=%d, y=%d", i, x1[i], y1[i]);
+		}
+
+		count2 = 0;
+		for(i=1;i<count-1;i++)
+		{
+			x_sum += x1[i];
+			y_sum += y1[i];
+			count2++;
+			printk("\t\t\t\t%d: x=%d, y=%d", i, x1[i], y1[i]);
+		}
+
+		x = x_sum / count2;
+		y = y_sum / count2;
+
+		if ((x > 4000) || (y > 4000) || (count <= 0))
+			return -4;
+
+		distance = int_sqrt((last_x-x)*(last_x-x) + (last_y-y)*(last_y-y));
+		printk("count=%d,x=%d,y=%d,last_j=%lu,current_j=%lu,diff=%lu,lastx=%d,lasty=%d, distance=%d,z1=%d,%d,%d", count, x, y, last_jiffies, jiffies, jiffies-last_jiffies, last_x, last_y, distance, z1[0], z1[1], z1[2]);
+#if 0
+		if((jiffies-last_jiffies <= 20) && (distance > 700))
+		{
+			printk("feidian 1");
+			return -5;
+		}
+		else if((jiffies-last_jiffies <= 100) && (distance > 1500))
+		{
+			printk("feidian 2");
+			return -5;
+		}
+#endif
 		if (!data->pen_down) {
+			printk("pen down!");
 			input_report_key(data->input, BTN_TOUCH, 1);
 			data->pen_down = true;
 		}
 
+		last_jiffies = jiffies;
+		last_x = x;
+		last_y = y;
+		printk("report end.");
 		input_report_abs(data->input, ABS_X, x);
 		input_report_abs(data->input, ABS_Y, y);
 		input_sync(data->input);
@@ -118,10 +243,47 @@ static void ns2009_ts_poll(struct input_polled_dev *dev)
 {
 	struct ns2009_data *data = dev->private;
 	int ret;
+	static int error = 0;
+
+	if(error == 1)
+	{
+		mdelay(10);
+		error = 0;
+	}
 
 	ret = ns2009_ts_report(data);
-	if (ret)
+	if (ret == -1)
+	{
 		dev_err(&dev->input->dev, "Poll touch data failed: %d\n", ret);
+		mdelay(10);
+	}
+	else if (ret == -5) 
+	{
+		printk("ret == -5");
+		input_report_key(data->input, BTN_TOUCH, 0);
+		input_sync(data->input);
+		data->pen_down = false;
+		//mdelay(100);
+		error = 1;
+	}
+	else if (ret == -4) 
+	{
+		printk("ret == -4");
+		input_report_key(data->input, BTN_TOUCH, 0);
+		input_sync(data->input);
+		data->pen_down = false;
+		//mdelay(100);
+		error = 1;
+	}
+	else if (ret == -3) 
+	{
+		printk("ret == -3");
+		input_report_key(data->input, BTN_TOUCH, 0);
+		input_sync(data->input);
+		data->pen_down = false;
+		//mdelay(100);
+		error = 1;
+	}
 }
 
 static void ns2009_ts_config_input_dev(struct ns2009_data *data)
